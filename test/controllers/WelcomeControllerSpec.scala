050bd4ea507827ba0c58773661188be037e83521
jake.barnes <jake.barnes@digital.hmrc.gov.uk>
2018-11-07 17:29:29 +0000

    "Session 4, greeter service functionality"

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -4,31 +4,49 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{status, _}
+import services.GreetingService
+
+object FakeMorningGreeter extends GreetingService {
+  override def greeting: String = "Good morning!"
+}
+
+object FakeAfternoonGreeter extends GreetingService {
+  override def greeting: String = "Good afternoon!"
+}
 
 class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
   "WelcomeController GET" should {
     "return a successful response" in {
-      val controller = new WelcomeController
-      val result = controller.welcome.apply(FakeRequest())
+      val controller = new WelcomeController(FakeMorningGreeter)
+      val result = controller.welcome.apply(FakeRequest(GET, "/welcome"))
       status(result) mustBe OK
     }
 
     "respond to the /welcome url" in {
+      // Need to specify Host header to get through AllowedHostsFilter
       val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
       val home = route(app, request).get
       status(home) mustBe OK
     }
 
     "return some html" in {
-      val controller = new WelcomeController
-      val result = controller.welcome.apply(FakeRequest())
+      val controller = new WelcomeController(FakeMorningGreeter)
+      val result = controller.welcome.apply(FakeRequest(GET, "/welcome"))
       contentType(result) mustBe Some("text/html")
     }
 
-    "say hello and have a title" in {
-      val controller = new WelcomeController
-      val result = controller.welcome.apply(FakeRequest(GET, "/foo"))
-      contentAsString(result) must include ("<h1>Hello!</h1>")
+    "say good morning and have a title" in {
+      val controller = new WelcomeController(FakeMorningGreeter)
+      val result = controller.welcome.apply(FakeRequest(GET, "/welcome"))
+      contentAsString(result) must include ("<h1>Good morning!</h1>")
+      contentAsString(result) must include ("<title>Welcome!</title>")
+    }
+
+    "say good afternoon when it's the afternoon and have a title" in {
+      val controller = new WelcomeController(FakeAfternoonGreeter)
+      val result = controller.welcome().apply(FakeRequest(GET, "/welcome"))
+      contentAsString(result) must not include ("<h1>Good morning!</h1>")
+      contentAsString(result) must include ("<h1>Good afternoon!</h1>")
       contentAsString(result) must include ("<title>Welcome!</title>")
     }
   }
